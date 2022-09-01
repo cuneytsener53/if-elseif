86a5cf33f5e083c8bb9541ef680c69891a7e0542
Cüneyt Şener <cuneytsener53@gmail.com>
2022-09-01 17:03:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time < 11)
+                Console.WriteLine("GÜnaydın");                
+
+            else if(time<= 18)
+                Console.WriteLine("İyi Günler :");
+
+            else
+                Console.WriteLine("İyi Geceler :");
+
+
+                string sonuc = time <=18 ? "İyi Günler": "İyi Geceler";
+
+                sonuc = time >=6 && time<11 ?"Günaydın" : time<= 18 ? "İyi günler" : "İyi geceler";
+                Console.WriteLine(sonuc);
+        }
+    }
+}
